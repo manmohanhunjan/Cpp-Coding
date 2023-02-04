473de16ebec752574e2eac14ed950d13e84ffbbf
Manmohan Singh Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-02-04 10:27:03 +0530

    Add files via upload

---
+++ Operators/daBeginning.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a=2/5;
+    cout<<a<<endl; //output: 0 Since int/int=int
+
+    int b = 2.0/5;
+    cout<<b<<endl; //output: 0 because we are putting data in an integer variable
+
+    cout<<2.0/5<<endl;
+
+    float c = 2.0/5;
+    cout<<c<<endl;
+
+
+}
