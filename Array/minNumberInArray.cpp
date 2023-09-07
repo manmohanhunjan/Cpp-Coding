e42150537ddb405fe8e12d0058f3621b508746f1
Manmohan Singh Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-09-07 16:26:56 +0530

    min Number Array added

---
+++ Array/minNumberInArray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+
+int findMinInArray(int arr[], int size){
+    int minAns = INT_MAX;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < minAns)
+        {
+            minAns = arr[i];
+        }
+    }
+    return minAns;
+}
+
+int main()
+{
+    int arr[] = {10, 8, 31, 4, 3, 1, 51};
+    int size = 7;
+    int minimum = findMinInArray(arr, size);
+    cout << "Minimum number is : " << minimum << endl;
+    return 0;
+}
